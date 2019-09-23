458f3723f651ff997cc907ce7e114c96c053cfe7
Pavel Rapoport <p.d.rapoport@gmail.com>
2019-09-23 17:40:14 +0200

    Creating simple mutations for while statements
    Appending negations for entire conditions in while statement.

--- javaParser.py
+++ javaParser.py
@@ -1,10 +1,10 @@
-import glob
 from pathlib import Path
 import subprocess
 
 
-# Looking into file for if statement and append negation for entire condition
-def openFileForIf(path):
+# Looking into file for if/while statements and append negation for entire condition
+def openFileForIfAndWhile(path):
+    subs = [' if ', ' while ']
     print('Formatting file ' + path)
     subprocess.call(['java', '-jar', 'google-java-format-1.7-all-deps.jar', '--replace', path])
     with open(path) as file:
@@ -12,34 +12,35 @@ def openFileForIf(path):
         print('File read')
         mutCount = 1
         for l in lines:
-            if ' if ' in l:
-                print('if statement found in string ' + l)
-                savedLine = l
-                index = lines.index(l)
-                lines[index] = insertExMarkInIf(l)
-                print('Writing mutation')
-                with open(path + '.mut' + "{}".format(mutCount), "w+") as mutation:
-                    mutation.writelines(lines)
-                mutCount += 1
-                lines[index] = savedLine
+            for sub in subs:
+                if sub in l:
+                    print(sub + ' statement found in string ' + l)
+                    savedLine = l
+                    index = lines.index(l)
+                    lines[index] = insertExMarkInIfAndWhile(l, sub)
+                    print('Writing mutation')
+                    with open(path + '.mut' + "{}".format(mutCount), "w+") as mutation:
+                        mutation.writelines(lines)
+                    mutCount += 1
+                    lines[index] = savedLine
         with open(path + '.orig', "w+") as orig:
             orig.writelines(lines)
 
 
 # Inserting negation for entire condition
-def insertExMarkInIf(line):
-    index = line.find(' if ')
+def insertExMarkInIfAndWhile(line, sub):
+    index = line.find(sub)
     print('Original: ' + line)
-    line = line[:index + 4] + '(!' + line[index + 4:len(line) - 3] + ')' + line[len(line) - 3:]
+    line = line[:index + len(sub)] + '(!' + line[index + len(sub):len(line) - 3] + ')' + line[len(line) - 3:]
     print('Modified: ' + line)
     return line
 
 
-# Get all .java files and create mutations for if statements
+# Get all .java files and create mutations for if/while statements
 def main():
     javaFiles = list(Path(".").rglob("*.java"))
     for file in javaFiles:
-        openFileForIf(file.absolute().as_posix())
+        openFileForIfAndWhile(file.absolute().as_posix())
 
 
 if __name__ == "__main__":
