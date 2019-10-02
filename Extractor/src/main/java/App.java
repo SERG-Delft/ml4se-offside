03a14ddcac94503e4c8bf4588b6e4ef74abf6330
Pavel Rapoport <p.d.rapoport@gmail.com>
2019-10-02 19:52:53 +0200

    Reworked code for parallel processing
    - All functions except for extractDir are moved to class ExtractionThread, since that all can be executed sequentially in the thread.
    - Amount of threads is implemented as argument for program
    - Threads accessing output file with ReentrantLock to avoid collision. Need to check if there is a faster solution to return result from threads.

--- Extractor/src/main/java/App.java
+++ Extractor/src/main/java/App.java
@@ -4,9 +4,13 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class App {
     private static String dirPath;
@@ -15,98 +19,160 @@ public class App {
     private static int maxContexts;
     private static String outPutDir;
 
-    public static void main(String[] args) {
-        dirPath = args[0];//Paths.get(System.getProperty("user.dir")).toString();
-        outPutDir = args[1];//"myfile.txt";
-        maxPathLength = args[2];//"8";
-        maxPathWidth = args[3];//"2";
-        maxContexts = Integer.parseInt(args[4]);//200;
+    private static Instant start;
+
+    private static int threadCount;
+
+    private static long totalCount = 0;
+
+    private static ReentrantLock failCountLock = new ReentrantLock();
+    private static int failCount = 0;
+
+    private static int[] count = {0};
+
+    private static ReentrantLock doneLock = new ReentrantLock();
+    private static int doneCount = 0;
+
+    private static ReentrantLock writerLock = new ReentrantLock();
+    private static PrintWriter outWriter;
+
+    private static void finalStats() {
+        long secondsElapsed = Duration.between(start, Instant.now()).getSeconds();
+        System.out.println("Task finished");
+        System.out.println("Total amount of files: " + totalCount);
+        System.out.println("Files failed to be parsed: " + failCount);
+        System.out.println("Time elapsed: " +
+                String.format("%d:%02d:%02d",
+                        secondsElapsed / 3600, (secondsElapsed % 3600) / 60, secondsElapsed % 60));
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        dirPath = args[0];
+        outPutDir = args[1];
+        maxPathLength = args[2];
+        maxPathWidth = args[3];
+        maxContexts = Integer.parseInt(args[4]);
+        threadCount = Integer.parseInt(args[5]);
+        start = Instant.now();
+        System.out.println("Starting parsing with " + threadCount + " threads");
+
+        outWriter = new PrintWriter(new BufferedWriter(new FileWriter(outPutDir, true)));
+
         extractDir();
     }
 
-    private static void extractDir() {
-        ExecutorService executor = Executors.newCachedThreadPool();
-        LinkedList<ExtractionTask> tasks = new LinkedList<>();
+    private static void extractDir() throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
         try {
+            totalCount = Files.walk(Paths.get(dirPath)).filter(Files::isRegularFile)
+                    .filter(path -> path.toString().toLowerCase().endsWith(".java")).count();
+
             Files.walk(Paths.get(dirPath)).filter(Files::isRegularFile)
-                    .filter(path -> path.toString().toLowerCase().endsWith(".java"))
-                    .forEach(path -> {
-                        ExtractionTask extractor = new ExtractionTask(path);
-                        printMethodsAndContextPathsToFile(extractor.processFile());
+                    .filter(path -> path.toString().toLowerCase().endsWith(".java")).
+                    forEach(path -> {
+                        Runnable extractor = new ExtractionThread(new ExtractionTask(path), count[0]);
+                        executor.execute(extractor);
+                        ++count[0];
                     });
         } catch (IOException e) {
             e.printStackTrace();
         }
+        executor.shutdown();
+        while (!executor.awaitTermination(10, TimeUnit.SECONDS)) {
+            doneLock.lock();
+            System.out.println("Awaiting completion. Completed: " + doneCount + " of " + totalCount);
+            doneLock.unlock();
+        }
+        finalStats();
     }
 
-    private static void extractContextPathFromFile(String type, String fileName) throws IOException {
-        Path currentPath = Paths.get(System.getProperty("user.dir"), "src", "main", "java");
-        Path contextExtractorPath = Paths.get(currentPath.toString(), "JavaExtractor-0.0.2-SNAPSHOT.jar");
-        ProcessBuilder pb = new ProcessBuilder("java", "-cp", contextExtractorPath.toString(), "JavaExtractor.App", "--max_path_length", maxPathLength, "--max_path_width", maxPathWidth, "--file", fileName, "--no_hash");
-        Process p = pb.start();
+    public static class ExtractionThread implements Runnable{
+        private ExtractionTask extractor;
+        private String fileNameTemplate;
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String contextPathForMethod;
-        ArrayList<String> lines = new ArrayList<>();
-        while ((contextPathForMethod = in.readLine()) != null) {
-            lines.add(contextPathForMethod);
+        public ExtractionThread(ExtractionTask extractor, int threadId) {
+            this.extractor = extractor;
+            this.fileNameTemplate = "correct" + threadId + ".txt";
         }
-        ArrayList<String> result = new ArrayList<>();
-        for (String line : lines) {
-            ArrayList<String> parts = new ArrayList<>(Arrays.asList(line.trim().split(" ")));
-            String methodName = parts.get(0);
-            ArrayList<String> currentResultLineParts = new ArrayList<>(Collections.singletonList(type));
-            parts.remove(0);
-            Iterator<String> partsIterator = parts.iterator();
-            int i = 0;
-            while (partsIterator.hasNext() && i < maxContexts) {
-                String[] contextParts = partsIterator.next().split(",");
-                String contextWord1 = contextParts[0];
-                String contextPath = contextParts[1];
-                String contextWord2 = contextParts[2];
-                currentResultLineParts.add(contextWord1 + "," + contextPath + "," + contextWord2);
-                i++;
-            }
-            String resultLine = String.join(" ", currentResultLineParts);
-            result.add(resultLine);
-        }
-        try (FileWriter fw = new FileWriter(outPutDir, true);
-             BufferedWriter bw = new BufferedWriter(fw);
-             PrintWriter out = new PrintWriter(bw)) {
-            for (String contextPath : result) {
-                out.println(contextPath);
-            }
-        } catch (IOException e) {
-            System.out.println("exception occoured: " + e);
-        }
-    }
 
-    public static void printMethodsAndContextPathsToFile(LinkedHashMap<String, List<MethodDeclaration>> methodsAndMutatedMethods) {
-        List<MethodDeclaration> methods = methodsAndMutatedMethods.get("0");
-        List<MethodDeclaration> mutatedMethods = methodsAndMutatedMethods.get("1");
-        try {
-            PrintWriter writer = new PrintWriter(new FileWriter("correct.txt", false));
+        @Override
+        public void run() {
+            try {
+                printMethodsAndContextPathsToFile(extractor.processFile());
+            } catch (Exception e) {
+                System.out.println("Failed to process file " + extractor.filePath);
+                failCountLock.lock();
+                doneLock.lock();
+                ++doneCount;
+                ++failCount;
+                doneLock.unlock();
+                failCountLock.unlock();
+            }
+        }
+
+        private void printMethodsAndContextPathsToFile(LinkedHashMap<String, List<MethodDeclaration>> methodsAndMutatedMethods) throws IOException {
+            List<MethodDeclaration> methods = methodsAndMutatedMethods.get("0");
+            List<MethodDeclaration> mutatedMethods = methodsAndMutatedMethods.get("1");
+
+            PrintWriter writer = new PrintWriter(new FileWriter(fileNameTemplate, false));
 
             for (MethodDeclaration method : methods) {
                 writer.println(method);
                 writer.println();
             }
             writer.close();
-            extractContextPathFromFile("0", "correct.txt");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            PrintWriter writer = new PrintWriter(new FileWriter("incorrect.txt", false));
+            extractContextPathFromFile("0", fileNameTemplate);
+
+            writer = new PrintWriter(new FileWriter("in" + fileNameTemplate, false));
 
             for (MethodDeclaration mutatedMethod : mutatedMethods) {
                 writer.println(mutatedMethod);
                 writer.println();
             }
             writer.close();
-            extractContextPathFromFile("0", "incorrect.txt");
-        } catch (Exception e) {
-            e.printStackTrace();
+            extractContextPathFromFile("0", "in" + fileNameTemplate);
+            doneLock.lock();
+            ++doneCount;
+            doneLock.unlock();
+        }
+
+        private void extractContextPathFromFile(String type, String fileName) throws IOException {
+            Path currentPath = Paths.get(System.getProperty("user.dir"), "src", "main", "java");
+            Path contextExtractorPath = Paths.get(currentPath.toString(), "JavaExtractor-0.0.2-SNAPSHOT.jar");
+            ProcessBuilder pb = new ProcessBuilder("java", "-cp", contextExtractorPath.toString(), "JavaExtractor.App", "--max_path_length", maxPathLength, "--max_path_width", maxPathWidth, "--file", fileName, "--no_hash");
+            Process p = pb.start();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String contextPathForMethod;
+            ArrayList<String> lines = new ArrayList<>();
+            while ((contextPathForMethod = in.readLine()) != null) {
+                lines.add(contextPathForMethod);
+            }
+            ArrayList<String> result = new ArrayList<>();
+            for (String line : lines) {
+                ArrayList<String> parts = new ArrayList<>(Arrays.asList(line.trim().split(" ")));
+                String methodName = parts.get(0);
+                ArrayList<String> currentResultLineParts = new ArrayList<>(Collections.singletonList(type));
+                parts.remove(0);
+                Iterator<String> partsIterator = parts.iterator();
+                int i = 0;
+                while (partsIterator.hasNext() && i < maxContexts) {
+                    String[] contextParts = partsIterator.next().split(",");
+                    String contextWord1 = contextParts[0];
+                    String contextPath = contextParts[1];
+                    String contextWord2 = contextParts[2];
+                    currentResultLineParts.add(contextWord1 + "," + contextPath + "," + contextWord2);
+                    i++;
+                }
+                String resultLine = String.join(" ", currentResultLineParts);
+                result.add(resultLine);
+            }
+            writerLock.lock();
+            for (String contextPath : result) {
+                outWriter.println(contextPath);
+            }
+            writerLock.unlock();
+            new File(fileName).delete();
         }
     }
 }
