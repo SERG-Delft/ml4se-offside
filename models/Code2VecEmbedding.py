b6382b75dc56e55323e955e7aa60b01b85edf790
J0rd1smit <jordismit20000@gmail.com>
2019-10-15 10:53:48 +0200

    Code2VecEmbedding

---
+++ models/Code2VecEmbedding.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+from typing import List, Set, Dict, Tuple, Optional, Union, Any, cast
+import tensorflow as tf
+from Config import Config
+from models.Code2VecCustomModel import Code2VecCustomModel
+from utils.Types import GraphInput
+import numpy as np
+
+def _assert_shape(x: tf.Tensor, shape: List[int]) -> None:
+    assert x.shape.as_list() == shape, f"Expected shape: {shape} but was: {x.shape.as_list()}"
+
+class Code2VecEmbedding(tf.keras.Model):
+    def __init__(
+            self,
+            config: Config,
+            **kwargs
+    ) -> None:
+        self.config = config
+        super(Code2VecEmbedding, self).__init__(**kwargs)
+
+        self.token_embedding_layer = tf.keras.layers.Embedding(config.N_TOKEN_EMBEDDINGS, config.TOKEN_EMBEDDINGS_SIZE,
+                                                               name="token_embedding_layer")
+        self.path_embedding_layer = tf.keras.layers.Embedding(config.N_PATH_EMBEDDINGS, config.PATH_EMBEDDINGS_SIZE,
+                                                              name="path_embedding_layer")
+
+    def call(
+            self,
+            inputs: Tuple[GraphInput, GraphInput, GraphInput, GraphInput],
+            **kwargs
+    ) -> tf.Tensor:
+        path_source_token_idxs, path_idxs, path_target_token_idxs, context_valid_masks = inputs
+
+        batch_size = path_source_token_idxs.shape[0]
+
+        assert batch_size == path_idxs.shape[0]
+        assert batch_size == path_target_token_idxs.shape[0]
+        assert batch_size == context_valid_masks.shape[0]
+
+        source_word_embed = self.token_embedding_layer(path_source_token_idxs)
+        _assert_shape(source_word_embed, [batch_size, self.config.MAX_CONTEXTS, self.config.TOKEN_EMBEDDINGS_SIZE])
+        path_embed = self.path_embedding_layer(path_idxs)
+        _assert_shape(path_embed, [batch_size, self.config.MAX_CONTEXTS, self.config.PATH_EMBEDDINGS_SIZE])
+        target_word_embed = self.token_embedding_layer(path_target_token_idxs)
+        _assert_shape(target_word_embed, [batch_size, self.config.MAX_CONTEXTS, self.config.TOKEN_EMBEDDINGS_SIZE])
+
+        context_embed = tf.concat([source_word_embed, path_embed, target_word_embed], axis=-1)
+        _assert_shape(context_embed, [batch_size, self.config.MAX_CONTEXTS, self.config.CODE_VECTOR_SIZE])
+
+        return context_embed
+
+
