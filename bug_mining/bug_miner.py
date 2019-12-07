bbd08afc3b24eab78aea64ef23f02f9c51cb1b02
Hendrig Sellik <sellikhendrig@gmail.com>
2019-12-07 20:46:56 +0100

    Add other types of bugs to bug mining

--- bug_mining/bug_miner.py
+++ bug_mining/bug_miner.py
@@ -10,7 +10,8 @@ local directory containing Java project repositories
 
 home = str(Path.home())
 # @TODO add this to configuration
-dataset_path = os.path.join(home, "ml4se_dataset", "mining")
+# Path containing GitHub repositories of Java projects
+dataset_path = os.path.join(home, "ml4se_dataset", "unique_mining")
 project_names = os.listdir(dataset_path)
 
 print("Starting to look through projects")
@@ -26,18 +27,44 @@ for project_name in project_names:
                     diff = modified_file.diff
                     parsed_diff = gr.parse_diff(diff)
 
-                    lines_containing_smaller_or_equal = []
+                    lines_containing_less_or_equal = []
+                    lines_containing_less = []
+                    lines_containing_greater_or_equal = []
+                    lines_containing_greater = []
                     for deletion in parsed_diff['deleted']:
                         line_nr = deletion[0]
                         content = deletion[1]
                         if " <= " in content:
-                            lines_containing_smaller_or_equal.append(line_nr)
+                            lines_containing_less_or_equal.append(line_nr)
+                        if " < " in content:
+                            lines_containing_less.append(line_nr)
+                        if " >= " in content:
+                            lines_containing_greater_or_equal.append(line_nr)
+                        if " > " in content:
+                            lines_containing_greater.append(line_nr)
 
                     for addition in parsed_diff['added']:
                         line_nr = addition[0]
                         content = addition[1]
-                        if line_nr in lines_containing_smaller_or_equal and " < " in content:
+                        # <= to <
+                        if line_nr in lines_containing_less_or_equal and " < " in content:
                             print(modified_file.filename + ":" + str(addition[0]))
+                            print("<= to <")
+                            print(gr.repo.remotes.origin.url[:-4] + "/commit/" + commit.hash)
+                        # < to <=
+                        if line_nr in lines_containing_less and " <= " in content:
+                            print(modified_file.filename + ":" + str(addition[0]))
+                            print("< to <=")
+                            print(gr.repo.remotes.origin.url[:-4] + "/commit/" + commit.hash)
+                        # >= to >
+                        if line_nr in lines_containing_greater_or_equal and " > " in content:
+                            print(modified_file.filename + ":" + str(addition[0]))
+                            print(">= to >")
+                            print(gr.repo.remotes.origin.url[:-4] + "/commit/" + commit.hash)
+                        # > to >=
+                        if line_nr in lines_containing_greater and " >= " in content:
+                            print(modified_file.filename + ":" + str(addition[0]))
+                            print("> to >=")
                             print(gr.repo.remotes.origin.url[:-4] + "/commit/" + commit.hash)
     except Exception as e:
         print("Could not scan project: {}. Skipping it.".format(project_name))
